ee03712833d9b94197374971745eedf69f388ca9
xpa <xpadev@gmail.com>
2022-11-25 00:29:33 +0900

    reinit: nextjs with typescript

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,3 +1,3 @@
 export default {
-    "{src}/**/*.{ts,tsx,js,jsx,json,scss,css}": [()=>"npm run format",()=>"npm run eslint:fix",()=>"npm run check-types"],
+    "**/*.{ts,tsx,js,jsx,json,scss,css}": [()=>"npm run format",()=>"npm run eslint:fix",()=>"npm run check-types"],
 };
