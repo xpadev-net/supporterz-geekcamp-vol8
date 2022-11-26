e1bca0fa78d42ef51ef3844ddfc8f433edb63909
xpa <xpadev@gmail.com>
2022-11-26 12:16:21 +0900

    fix: type definition

--- @types/tasks.d.ts
+++ @types/tasks.d.ts
@@ -1,10 +1,18 @@
-type Task = {
+type TaskNoChild = {
   type: "task";
   title: string;
   content: string;
   expired?: number;
-  childTask?: Task[];
+  progress: number;
 };
+type TaskChild = {
+  type: "task";
+  title: string;
+  content: string;
+  expired?: number;
+  childTask: Task[];
+};
+type Task = TaskNoChild | TaskChild;
 type Tab = {
   type: "tab";
   title: string;
