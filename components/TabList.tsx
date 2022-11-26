6109c48d3b30d58d39de05abd49e3ec2a7092b4f
yasu37 <ykan4249@gmail.com>
2022-11-26 17:40:09 +0900

    second commit

--- components/TabList.tsx
+++ components/TabList.tsx
@@ -1,7 +1,22 @@
+import { useContext } from "react";
+import { tasksContext } from "@/contexts/tasks";
+import Styles from "@/styles/components/TabList.module.scss";
+
 type props = {
   tabs: Tab[];
 };
 const TabList = ({ tabs }: props) => {
-  return <></>;
+  const { isMobile } = useContext(tasksContext);
+
+  return (
+    <div>
+      <ul className="TabList">
+        {tabs.map((tab) => <li>{tab.title}</li>)}
+        <li>
+          <button>+</button>
+        </li>
+      </ul>
+    </div>
+  )
 };
 export { TabList };
