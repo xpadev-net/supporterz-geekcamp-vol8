7b2924b1a8eff95c4d676f75dff4bf9a39433156
SadaharuKishi <harupinpan3@gmail.com>
2022-11-26 19:56:12 +0900

    TSサイコー!!

--- components/Task.tsx
+++ components/Task.tsx
@@ -3,37 +3,16 @@ import React, { useEffect, useState } from "react";
 type props = {
   task: Task;
 };
+
 const Task = ({ task }: props) => {
-  return <></>;
-};
-export { Task };
-
-export const TestUseState = () => {
-  const [TaskName, setTaskName] = useState<string>();
-  const [TaskMonth, setTaskMonth] = useState<string>();
-  const [TaskDate, setTaskDate] = useState<string>();
-  const [TaskDesc, setTaskDesc] = useState<string>();
-
-  useEffect(() => {
-    setTaskName("Task1");
-    setTaskMonth("1");
-    setTaskDate("1");
-    setTaskDesc("ここにタスクの内容を入れてください");
-  }, []);
-
   return (
-    <div className="m-3 col-4">
-      <div className="card">
-        <div className="card-body">
-          <div>タスク：{TaskName}</div>
-          <div>
-            {TaskMonth}/{TaskDate}
-          </div>
-          <p>{TaskDesc}</p>
-        </div>
+    <div className="card">
+      <div className="card-body">
+        <div className="font-weight-bold">{task.title}</div>
+        <p>{task.content}</p>
       </div>
     </div>
   );
 };
 
-export default TestUseState;
+export { Task };
