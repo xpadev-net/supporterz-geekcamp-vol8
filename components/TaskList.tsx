63807da95f2214664141ef1562bc63f3279b6753
YabuYuki <yuuki_624@icloud.com>
2022-11-27 00:42:11 +0900

    SecondCommit

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,7 +1,43 @@
-type props = {
-  tasks: Task[];
-};
-const TaskList = ({ tasks }: props) => {
-  return <></>;
-};
-export { TaskList };
+import React from "react";
+import { Task } from "./Task";
+
+// type props = {
+//   tasks: Task[];
+//   task: Task;
+// };
+
+// TaskItemチェックボックスとか削除ボタン
+// const TaskItem: React.FC<props> = ({ task }) => {
+//   return (
+//     <li>
+//       <label>
+//         <input type="checkbox" className="checkbox-input" />
+//         <span className="checkbox-label">{task.title}</span>
+//         <button className="btn is-deleted">削除</button>
+//       </label>
+//     </li>
+//   )
+// }
+
+// TaskListの表示
+// const TaskList: React.FC<props> = ({ tasks }) => {
+//   return <>
+//     <div className="inner">
+//       {
+//         tasks.length <= 0 ? "登録されていません" :
+//           <ul className="task-list">
+//             {
+//               tasks.map(task => (
+//                 <TaskItem
+//                   key={task.expired}
+//                   task={task}
+//                 />
+
+//               ))
+//             }
+//           </ul>
+//       }
+//     </div>
+//   </>;
+// };
+// export { TaskList };
