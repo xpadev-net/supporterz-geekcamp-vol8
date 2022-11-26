e3e7c80f558395fbea869d20dc25de30f2b6a0ab
SadaharuKishi <harupinpan3@gmail.com>
2022-11-26 22:34:56 +0900

    ざっくり概形を書いてみました。

--- components/Task.tsx
+++ components/Task.tsx
@@ -1,16 +1,20 @@
-import React, { useEffect, useState } from "react";
-
 type props = {
   task: Task;
 };
 
 const Task = ({ task }: props) => {
   return (
-    <div className="card">
-      <div className="card-body">
-        <div className="font-weight-bold">{task.title}</div>
-        <p>{task.content}</p>
+    <div className="task-box">
+      <div className="taskTitle-box">
+        <div className="taskTitle-style">{task.title}</div>
       </div>
+      <div className="taskExp-box">
+        <div className="taskExp-style">task.expired</div>
+      </div>
+      <div className="taskCont-box">
+        <div className="taskCont-style">{task.content}</div>
+      </div>
+      <div>task.progress</div>
     </div>
   );
 };
