c36d67c839275388c2040a8ee9fa7dbb98f636ad
YabuYuki <yuuki_624@icloud.com>
2022-11-27 13:59:33 +0900

    タスクをクリックしたらアラートが出るようにしました

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,43 +1,74 @@
 import React from "react";
-import { Task } from "./Task";
 
-// type props = {
-//   tasks: Task[];
-//   task: Task;
-// };
+
+type props = {
+    tasks: Task[];
+    task: Task;
+};
 
 // TaskItemチェックボックスとか削除ボタン
-// const TaskItem: React.FC<props> = ({ task }) => {
-//   return (
-//     <li>
-//       <label>
-//         <input type="checkbox" className="checkbox-input" />
-//         <span className="checkbox-label">{task.title}</span>
-//         <button className="btn is-deleted">削除</button>
-//       </label>
-//     </li>
-//   )
-// }
+function TaskItem(task: props) {
+    return (
+        <li>
+            {/* チェックボックス */}
+            <label>
+                <input type="checkbox" className="checkbox-input" />
+                <span className="checkbox-label">{task.title}</span>
+            </label>
+            {/* 削除ボタン */}
+            <button className="btn is-deleted">削除</button>
+        </li>
+    )
+}
+
+const TaskList = ({ tasks }: props) => {
+
+    const list = tasks.map((task) =>
+        <TaskItem
+            key={task.title}
+            task={task}
+        />
+    );
+
+    function ClickList() {
+        alert(list);
+    }
+
+    return <>
+        <div className="TaskList">
+            {
+                tasks.length <= 0 ? "登録されていません" :
+                    <ol>
+                        <li onClick={ClickList}>{list}</li>
+                    </ol>
+            }
+        </div>
+    </>;
+};
+
+export { TaskList };
+
+
 
 // TaskListの表示
 // const TaskList: React.FC<props> = ({ tasks }) => {
-//   return <>
-//     <div className="inner">
-//       {
-//         tasks.length <= 0 ? "登録されていません" :
-//           <ul className="task-list">
+//     return <>
+//         <div className="inner">
 //             {
-//               tasks.map(task => (
-//                 <TaskItem
-//                   key={task.expired}
-//                   task={task}
-//                 />
+//                 tasks.length <= 0 ? "登録されていません" :
+//                     <ul className="task-list">
+//                         {
+//                             tasks.map(task => (
+//                                 <TaskItem
+//                                     key={task.expired}
+//                                     task={task}
+//                                 />
 
-//               ))
+//                             ))
+//                         }
+//                     </ul>
 //             }
-//           </ul>
-//       }
-//     </div>
-//   </>;
+//         </div>
+//     </>;
 // };
 // export { TaskList };
