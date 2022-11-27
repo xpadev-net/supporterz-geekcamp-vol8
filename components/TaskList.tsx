681d6077c2d60f304c03a6b301e5217eb793cc6a
xpa <xpadev@gmail.com>
2022-11-27 14:57:21 +0900

    fix: conflict

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 
 type props = {
-<<<<<<< HEAD
   tasks: Task[];
-  task: Task;
-=======
-    tasks: Task[];
->>>>>>> 1b35be3 (修正したやつです)
 };
 
-type TaskItempronps = { task: Task; }
+type TaskItempronps = { task: Task };
 // TaskItemチェックボックスとか削除ボタン
-<<<<<<< HEAD
-function TaskItem(task: props) {
+function TaskItem({ task }: TaskItempronps) {
   return (
     <li>
       {/* チェックボックス */}
@@ -24,26 +18,15 @@ function TaskItem(task: props) {
       <button className="btn is-deleted">削除</button>
     </li>
   );
-=======
-function TaskItem({ task }: TaskItempronps) {
-    return (
-        <li>
-            {/* チェックボックス */}
-            <label>
-                <input type="checkbox" className="checkbox-input" />
-                <span className="checkbox-label">{task.title}</span>
-            </label>
-            {/* 削除ボタン */}
-            <button className="btn is-deleted">削除</button>
-        </li>
-    )
->>>>>>> 1b35be3 (修正したやつです)
 }
 
 const TaskList = ({ tasks }: props) => {
-  const list = tasks.map((task) => <TaskItem key={task.title} task={task} />);
+  const list = tasks.map((task) => (
+    <li key={task.title}>
+      <TaskItem task={task} />
+    </li>
+  ));
 
-<<<<<<< HEAD
   function ClickList() {
     alert(list);
   }
@@ -51,40 +34,10 @@ const TaskList = ({ tasks }: props) => {
   return (
     <>
       <div className="TaskList">
-        {tasks.length <= 0 ? (
-          "登録されていません"
-        ) : (
-          <ol>
-            <li onClick={ClickList}>{list}</li>
-          </ol>
-        )}
+        {tasks.length <= 0 ? "登録されていません" : <ol>{list}</ol>}
       </div>
     </>
   );
-=======
-    const list = tasks.map((task) =>
-        <li key={task.title}>
-            <TaskItem
-
-                task={task} />
-        </li>
-    );
-
-    function ClickList() {
-        alert(list);
-    }
-
-    return <>
-        <div className="TaskList">
-            {
-                tasks.length <= 0 ? "登録されていません" :
-                    <ol>
-                        {list}
-                    </ol>
-            }
-        </div>
-    </>;
->>>>>>> 1b35be3 (修正したやつです)
 };
 
 export { TaskList };
