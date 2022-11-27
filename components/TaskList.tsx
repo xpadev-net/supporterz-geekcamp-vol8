7237ce3726a06bc27eea90b9257ee5b716f9b7b5
YabuYuki <yuuki_624@icloud.com>
2022-11-27 14:49:37 +0900

    修正したやつです

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 
 type props = {
+<<<<<<< HEAD
   tasks: Task[];
   task: Task;
+=======
+    tasks: Task[];
+>>>>>>> 1b35be3 (修正したやつです)
 };
 
+type TaskItempronps = { task: Task; }
 // TaskItemチェックボックスとか削除ボタン
+<<<<<<< HEAD
 function TaskItem(task: props) {
   return (
     <li>
@@ -18,11 +24,26 @@ function TaskItem(task: props) {
       <button className="btn is-deleted">削除</button>
     </li>
   );
+=======
+function TaskItem({ task }: TaskItempronps) {
+    return (
+        <li>
+            {/* チェックボックス */}
+            <label>
+                <input type="checkbox" className="checkbox-input" />
+                <span className="checkbox-label">{task.title}</span>
+            </label>
+            {/* 削除ボタン */}
+            <button className="btn is-deleted">削除</button>
+        </li>
+    )
+>>>>>>> 1b35be3 (修正したやつです)
 }
 
 const TaskList = ({ tasks }: props) => {
   const list = tasks.map((task) => <TaskItem key={task.title} task={task} />);
 
+<<<<<<< HEAD
   function ClickList() {
     alert(list);
   }
@@ -40,6 +61,30 @@ const TaskList = ({ tasks }: props) => {
       </div>
     </>
   );
+=======
+    const list = tasks.map((task) =>
+        <li key={task.title}>
+            <TaskItem
+
+                task={task} />
+        </li>
+    );
+
+    function ClickList() {
+        alert(list);
+    }
+
+    return <>
+        <div className="TaskList">
+            {
+                tasks.length <= 0 ? "登録されていません" :
+                    <ol>
+                        {list}
+                    </ol>
+            }
+        </div>
+    </>;
+>>>>>>> 1b35be3 (修正したやつです)
 };
 
 export { TaskList };
